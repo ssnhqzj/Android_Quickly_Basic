5b0997ff618b426dab508b9e16e91eefc6a474ae
qzj <345412682@qq.com>
2016-01-27 15:22:49 +0800

    友盟社会化分享库

---
+++ umeng/src/main/java/qzj/com/umeng/platforms/wxapi/WXEntryActivity.java
@@ -0,0 +1,9 @@
+package qzj.com.umeng.platforms.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by ntop on 15/9/4.
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+}
